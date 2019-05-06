a6a99a4f8e044cf20e86df739caffc973bad7566
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-06 17:45:29 +0300

    Sixth Commit
    Deleting and Updating Objects

--- CodeFirst_Existing_Database/Program.cs
+++ CodeFirst_Existing_Database/Program.cs
@@ -11,24 +11,43 @@ namespace CodeFirst_Existing_Database
         static void Main(string[] args)
         {
 
-            // Adding Objects
-            var context = new PlutoContext();
+            ////////////////////////////////////////////// Adding Objects //////////////////////////////////////////////
+            //var context = new PlutoContext();
 
-            var authors = context.Authors.ToList();
+            //var authors = context.Authors.ToList();
 
-            var author = context.Authors.Single(a => a.Id == 1);
-            var course = new Courses
-            {
-                Name = "New Course",
-                Description = "New Description",
-                FullPrice = 19.95f,
-                level = 1,
-                Authors = author
-            };
+            //var author = context.Authors.Single(a => a.Id == 1);
+            //var course = new Courses
+            //{
+            //    Name = "New Course",
+            //    Description = "New Description",
+            //    FullPrice = 19.95f,
+            //    level = 1,
+            //    Authors = author
+            //};
 
-            context.Courses.Add(course);
+            //context.Courses.Add(course);
 
-            context.SaveChanges();
+            //context.SaveChanges();
+
+            ////////////////////////////////////////////// Updating Objects //////////////////////////////////////////////
+            //var context = new PlutoContext();
+
+            //var course = context.Courses.Find(4);
+            //course.Name = "Mustafa Alparslan Pamuk";
+            //course.Author_Id = 2;
+            //context.SaveChanges();
+
+            ////////////////////////////////////////////// Deleting Objects //////////////////////////////////////////////
+
+            //var context = new PlutoContext();
+
+            //var authors = context.Authors.Include(a => a.Courses).Single(a => a.Id == 2);
+            //context.Courses.RemoveRange(authors.Courses);
+            //context.Authors.Remove(authors);
+
+            //context.SaveChanges();
+
         }
     }
 }
