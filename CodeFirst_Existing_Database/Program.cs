2811d2036111698e8068fcbb435dc0a2b70e094d
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-06 16:11:09 +0300

    Fifth Commit
    Adding Objects

--- CodeFirst_Existing_Database/Program.cs
+++ CodeFirst_Existing_Database/Program.cs
@@ -10,6 +10,25 @@ namespace CodeFirst_Existing_Database
     {
         static void Main(string[] args)
         {
+
+            // Adding Objects
+            var context = new PlutoContext();
+
+            var authors = context.Authors.ToList();
+
+            var author = context.Authors.Single(a => a.Id == 1);
+            var course = new Courses
+            {
+                Name = "New Course",
+                Description = "New Description",
+                FullPrice = 19.95f,
+                level = 1,
+                Authors = author
+            };
+
+            context.Courses.Add(course);
+
+            context.SaveChanges();
         }
     }
 }
